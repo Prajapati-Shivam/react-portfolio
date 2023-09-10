5038bbc197b2cc461945511aa6ee22a2cfdee858
Prajapati-Shivam <prajapatishivam11203@gmail.com>
2023-09-10 15:23:56 +0530

    project info changed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import Project from "./Project";
 import cloudbook from "../assets/cloudbook.png";
 import imagery from "../assets/imagery.png";
-import stock from "../assets/stock.png";
+import stockwise from "../assets/stockwise.png";
 import horizon from "../assets/horizon.png";
 import dailynews from "../assets/dailynews.png";
 import weather from "../assets/weather.png";
@@ -20,36 +20,36 @@ const projects = [
     image: imagery,
     code: "https://github.com/Prajapati-Shivam/dall-e-clone",
     demo: "https://imagery-dall-e.vercel.app/",
-    desc: "Imagery is a dall-e clone to generate AI images built with mern stack"
+    desc: "Imagery is a dall-e clone to generate AI images built with mern stack",
   },
   {
     title: "Horizon",
     image: horizon,
     code: "https://github.com/Prajapati-Shivam/Horizon",
     demo: "https://prajapati-shivam.github.io/Horizon/",
-    desc: "Simple E-commerce website built with html, css and javascript."
+    desc: "Simple E-commerce website built with html, css and javascript.",
   },
   {
     title: "DailyNews",
     image: dailynews,
     code: "https://github.com/Prajapati-Shivam/DailyNews",
     demo: "https://daily-news-shivam.netlify.app/",
-    desc: "React app for daily news build with newsdata.io API."
+    desc: "React app for daily news build with newsdata.io API.",
   },
   {
     title: "Inventory Management System",
-    image: stock,
-    code: "https://github.com/Prajapati-Shivam/Inventory-Management-System",
-    demo: "https://stock-management-system.netlify.app",
-    desc: "Inventory Management System is a simple web application built using Nextjs and MongoDB. Users can easily create and delete products and update their quantity and price."
+    image: stockwise,
+    code: "https://github.com/Prajapati-Shivam/Stockwise",
+    demo: "https://stockwise.vercel.app",
+    desc: "Inventory Management System is a simple web application built using Nextjs and MongoDB. Users can easily create and delete products and update their quantity and price.",
   },
   {
     title: "Weather App",
     image: weather,
     code: "https://github.com/Prajapati-Shivam/Weather-App",
     demo: "https://prajapati-shivam.github.io/Weather-App/",
-    desc: "Weather app made with help of openweathermap API to get the weather details of different places."
-  }
+    desc: "Weather app made with help of openweathermap API to get the weather details of different places.",
+  },
 ];
 export default function Projects() {
   return (
